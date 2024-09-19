a0380c2de7da0711b1f82366b8f60aa6c0a92888
PhilippKDT <p.kleidt@mbd-team.de>
2024-09-19 12:29:36 +0200

    food generation is working

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,8 @@ import "./style.css";
 const playground = <HTMLDivElement>document.getElementById("playground");
 
 let liveSnakePosition = 113;
-const snakeTracker = {
-  x: 8,
-  y: 8,
-};
+
+let foodPosition = foodGenerator();
 
 function renderingPlayfield() {
   playground.innerHTML = "";
@@ -15,6 +13,11 @@ function renderingPlayfield() {
       const div = document.createElement("div");
       div.id = `div${i.toString()}`;
       div.className = "snake";
+      playground.appendChild(div);
+    } else if (i === foodPosition) {
+      const div = document.createElement("div");
+      div.id = `div${i.toString()}`;
+      div.className = "food";
       playground.appendChild(div);
     } else {
       const div = document.createElement("div");
@@ -30,28 +33,46 @@ window.addEventListener("keydown", (event) => {
 });
 
 renderingPlayfield();
-newFoodLocation();
 
 function move(input: string) {
   if (input === "ArrowUp") {
     liveSnakePosition -= 15;
-    snakeTracker.y -= 1;
+    checkIfSnakeOnFood();
     renderingPlayfield();
   } else if (input === "ArrowDown") {
     liveSnakePosition += 15;
-    snakeTracker.y += 1;
+    checkIfSnakeOnFood();
     renderingPlayfield();
   } else if (input === "ArrowRight") {
     liveSnakePosition += 1;
-    snakeTracker.x += 1;
+    checkIfSnakeOnFood();
     renderingPlayfield();
   } else if (input === "ArrowLeft") {
     liveSnakePosition -= 1;
-    snakeTracker.x -= 1;
+    checkIfSnakeOnFood();
     renderingPlayfield();
   }
-  console.log(liveSnakePosition);
-  console.log(snakeTracker);
+}
+
+function foodGenerator() {
+  const randomLocationForX = Math.round(Math.random() * (15 - 1) + 1);
+  const randomLocationForY = Math.round(Math.random() * (15 - 1) + 1);
+  if (randomLocationForY === 1) {
+    const squaryApple = randomLocationForX;
+    return squaryApple;
+  } else {
+    const repairAppleCoordinates = randomLocationForY * 15;
+    const squaryApple = randomLocationForX + repairAppleCoordinates;
+    return squaryApple;
+  }
+}
+
+function checkIfSnakeOnFood() {
+  if (liveSnakePosition === foodPosition) {
+    foodPosition = 0;
+    const newPosition = foodGenerator();
+    foodPosition = newPosition;
+  }
 }
 
 // function checkIfBorderCrash() {
@@ -64,29 +85,3 @@ function move(input: string) {
 //     console.log("You loose");
 //   }
 // }
-
-function newFoodLocation() {
-  const randomLocationForX = Math.round(Math.random() * (15 - 1) + 1);
-  const randomLocationForY = Math.round(Math.random() * (15 - 1) + 1);
-  const foodPosition = {
-    foodX: randomLocationForX,
-    foodY: randomLocationForY,
-  };
-
-  if (randomLocationForY === 1) {
-    const squaryApple = randomLocationForX;
-    const foodId = "div" + squaryApple.toString();
-    const change = <HTMLDivElement>document.getElementById(foodId);
-    change.classList.add("food");
-  } else {
-    const repairAppleCoordinates = randomLocationForY * 15;
-    const squaryApple = randomLocationForX + repairAppleCoordinates;
-    const foodId = "div" + squaryApple.toString();
-    const change = <HTMLDivElement>document.getElementById(foodId);
-    change.classList.add("food");
-  }
-
-  const checkList = document.querySelectorAll(".food");
-  console.log(checkList);
-  console.log(foodPosition);
-}
