0b99f6ba5209a4d7cb322cfcad0dde3b34b0d80b
PhilippKDT <p.kleidt@mbd-team.de>
2024-09-19 11:20:32 +0200

    rendering function created. now rendering map new everytime. doesn´t work fully

--- src/main.ts
+++ src/main.ts
@@ -2,139 +2,68 @@ import "./style.css";
 
 const playground = <HTMLDivElement>document.getElementById("playground");
 
-const liveSnakePosition = {
+let liveSnakePosition = 113;
+const snakeTracker = {
   x: 8,
   y: 8,
 };
 
-let foodCounter = 0;
-
-let playerStart = "";
-
-for (let i = 1; i <= 225; i++) {
-  if (i === 113) {
-    const div = document.createElement("div");
-    div.id = `div${i.toString()}`;
-    playerStart = div.id;
-    div.className = "snake";
-    playground.appendChild(div);
-  } else {
-    const div = document.createElement("div");
-    div.id = `div${i.toString()}`;
-    playground.appendChild(div);
-  }
-}
-
-addFirstFood();
-
-window.addEventListener("keydown", (event) => {
-  move(event.key);
-});
-
-function newSnakePosition(inputCheck: string) {
-  const currentSnake = document.getElementsByClassName("snake");
-
-  for (let k = 0; k < currentSnake.length; k++) {
-    currentSnake[k].classList.remove("snake");
-    if (inputCheck === "ArrowUp") {
-      let storage = playerStart.split("div");
-      let nextStorage = storage[1];
-      let anotherStorage = parseInt(nextStorage);
-      let finalStorage = anotherStorage - 15;
-      let overwritten = finalStorage.toString();
-      let nextStep = "div" + overwritten;
-      const result = <HTMLDivElement>document.getElementById(nextStep);
-      result.classList.add("snake");
-      storage = [""];
-      nextStorage = "";
-      anotherStorage = 0;
-      finalStorage = 0;
-      overwritten = "";
-      playerStart = nextStep;
-      nextStep = "";
-    }
-    if (inputCheck === "ArrowDown") {
-      let storage = playerStart.split("div");
-      let nextStorage = storage[1];
-      let anotherStorage = parseInt(nextStorage);
-      let finalStorage = anotherStorage + 15;
-      let overwritten = finalStorage.toString();
-      let nextStep = "div" + overwritten;
-      const result = <HTMLDivElement>document.getElementById(nextStep);
-      result.classList.add("snake");
-      storage = [""];
-      nextStorage = "";
-      anotherStorage = 0;
-      finalStorage = 0;
-      overwritten = "";
-      playerStart = nextStep;
-      nextStep = "";
-    }
-    if (inputCheck === "ArrowRight") {
-      let storage = playerStart.split("div");
-      let nextStorage = storage[1];
-      let anotherStorage = parseInt(nextStorage);
-      let finalStorage = anotherStorage + 1;
-      let overwritten = finalStorage.toString();
-      let nextStep = "div" + overwritten;
-      const result = <HTMLDivElement>document.getElementById(nextStep);
-      result.classList.add("snake");
-      storage = [""];
-      nextStorage = "";
-      anotherStorage = 0;
-      finalStorage = 0;
-      overwritten = "";
-      playerStart = nextStep;
-      nextStep = "";
-    }
-    if (inputCheck === "ArrowLeft") {
-      let storage = playerStart.split("div");
-      let nextStorage = storage[1];
-      let anotherStorage = parseInt(nextStorage);
-      let finalStorage = anotherStorage - 1;
-      let overwritten = finalStorage.toString();
-      let nextStep = "div" + overwritten;
-      const result = <HTMLDivElement>document.getElementById(nextStep);
-      result.classList.add("snake");
-      storage = [""];
-      nextStorage = "";
-      anotherStorage = 0;
-      finalStorage = 0;
-      overwritten = "";
-      playerStart = nextStep;
-      nextStep = "";
+function renderingPlayfield() {
+  for (let i = 1; i <= 225; i++) {
+    if (i === liveSnakePosition) {
+      const div = document.createElement("div");
+      div.id = `div${i.toString()}`;
+      div.className = "snake";
+      playground.appendChild(div);
+    } else {
+      const div = document.createElement("div");
+      div.id = `div${i.toString()}`;
+      playground.appendChild(div);
     }
   }
-  checkIfBorderCrash();
+  window.addEventListener("keydown", (event) => {
+    move(event.key);
+  });
 }
+
+renderingPlayfield();
+newFoodLocation();
 
 function move(input: string) {
   if (input === "ArrowUp") {
-    liveSnakePosition.y -= 1;
+    liveSnakePosition -= 15;
+    snakeTracker.y -= 1;
+    renderingPlayfield();
   } else if (input === "ArrowDown") {
-    liveSnakePosition.y += 1;
+    liveSnakePosition += 15;
+    snakeTracker.y += 1;
+    renderingPlayfield();
   } else if (input === "ArrowRight") {
-    liveSnakePosition.x += 1;
+    liveSnakePosition += 1;
+    snakeTracker.x += 1;
+    renderingPlayfield();
   } else if (input === "ArrowLeft") {
-    liveSnakePosition.x -= 1;
+    liveSnakePosition -= 1;
+    snakeTracker.x -= 1;
+    renderingPlayfield();
   }
-  newSnakePosition(input);
-  checkIfSnakeOnFood();
+  // newSnakePosition(input);
   console.log(liveSnakePosition);
+  console.log(snakeTracker);
 }
 
-function checkIfBorderCrash() {
-  if (
-    liveSnakePosition.x === 16 ||
-    liveSnakePosition.x === 0 ||
-    liveSnakePosition.y === 16 ||
-    liveSnakePosition.y === 0
-  ) {
-    console.log("You loose");
-  }
-}
+// function checkIfBorderCrash() {
+//   if (
+//     snakeTracker.x === 16 ||
+//     snakeTracker.x === 0 ||
+//     snakeTracker.y === 16 ||
+//     snakeTracker.y === 0
+//   ) {
+//     console.log("You loose");
+//   }
+// }
 
-function checkIfSnakeOnFood() {
+function newFoodLocation() {
   const randomLocationForX = Math.round(Math.random() * (15 - 1) + 1);
   const randomLocationForY = Math.round(Math.random() * (15 - 1) + 1);
   const foodPosition = {
@@ -142,66 +71,8 @@ function checkIfSnakeOnFood() {
     foodY: randomLocationForY,
   };
 
-  if (
-    foodPosition.foodX === randomLocationForX &&
-    foodPosition.foodY === randomLocationForY
-  ) {
-    if (
-      liveSnakePosition.x === foodPosition.foodX &&
-      liveSnakePosition.y === foodPosition.foodY
-    ) {
-      if (randomLocationForY === 1) {
-        const repairAppleCoordinates = randomLocationForY;
-        const squaryApple = randomLocationForX + repairAppleCoordinates;
-        const foodId = "div" + squaryApple.toString();
-        const change = <HTMLDivElement>document.getElementById(foodId);
-        change.classList.remove("food");
-        foodPosition.foodX = 0;
-        foodPosition.foodY = 0;
-        foodCounter += 1;
-        checkIfSnakeOnFood();
-      } else {
-        const repairAppleCoordinates = randomLocationForY * 15;
-        const squaryApple = randomLocationForX + repairAppleCoordinates;
-        const foodId = "div" + squaryApple.toString();
-        const change = <HTMLDivElement>document.getElementById(foodId);
-        change.classList.remove("food");
-        foodPosition.foodX = 0;
-        foodPosition.foodY = 0;
-        foodCounter += 1;
-        checkIfSnakeOnFood();
-      }
-    } else {
-    }
-  } else if (foodPosition.foodX === 0 && foodPosition.foodY === 0) {
-    foodPosition.foodX = randomLocationForX;
-    foodPosition.foodY = randomLocationForY;
-    if (randomLocationForY === 1) {
-      const repairAppleCoordinates = randomLocationForY;
-      const squaryApple = randomLocationForX + repairAppleCoordinates;
-      const foodId = "div" + squaryApple.toString();
-      const change = <HTMLDivElement>document.getElementById(foodId);
-      change.classList.remove("food");
-    } else {
-      const repairAppleCoordinates = randomLocationForY * 15;
-      const squaryApple = randomLocationForX + repairAppleCoordinates;
-      const foodId = "div" + squaryApple.toString();
-      const change = <HTMLDivElement>document.getElementById(foodId);
-      change.classList.remove("food");
-    }
-  }
-}
-
-function addFirstFood() {
-  const randomLocationForX = Math.round(Math.random() * (15 - 1) + 1);
-  const randomLocationForY = Math.round(Math.random() * (15 - 1) + 1);
-  const foodPosition = {
-    foodX: randomLocationForX,
-    foodY: randomLocationForY,
-  };
   if (randomLocationForY === 1) {
-    const repairAppleCoordinates = randomLocationForY;
-    const squaryApple = randomLocationForX + repairAppleCoordinates;
+    const squaryApple = randomLocationForX;
     const foodId = "div" + squaryApple.toString();
     const change = <HTMLDivElement>document.getElementById(foodId);
     change.classList.add("food");
@@ -212,4 +83,33 @@ function addFirstFood() {
     const change = <HTMLDivElement>document.getElementById(foodId);
     change.classList.add("food");
   }
+
+  const checkList = document.querySelectorAll(".food");
+  console.log(checkList);
+  console.log(foodPosition);
 }
+
+// if (
+//   liveSnakePosition.x === foodPosition.foodX &&
+//   liveSnakePosition.y === foodPosition.foodY
+// ) {
+//   if (randomLocationForY === 1) {
+//     const repairAppleCoordinates = randomLocationForY;
+//     const squaryApple = randomLocationForX + repairAppleCoordinates;
+//     const foodId = "div" + squaryApple.toString();
+//     const change = <HTMLDivElement>document.getElementById(foodId);
+//     change.classList.remove("food");
+//     foodPosition.foodX = 0;
+//     foodPosition.foodY = 0;
+//     foodCounter += 1;
+//   } else {
+//     const repairAppleCoordinates = randomLocationForY * 15;
+//     const squaryApple = randomLocationForX + repairAppleCoordinates;
+//     const foodId = "div" + squaryApple.toString();
+//     const change = <HTMLDivElement>document.getElementById(foodId);
+//     change.classList.remove("food");
+//     foodPosition.foodX = 0;
+//     foodPosition.foodY = 0;
+//     foodCounter += 1;
+//   }
+// }
